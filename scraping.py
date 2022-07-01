9c7644e08a487129454181277561684156dc56bd
jenchick <jchickow@gmail.com>
2022-06-30 22:29:41 -0500

    Finishing Module 10

--- scraping.py
+++ scraping.py
@@ -97,7 +97,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html(classes="table table-dark" )
 
 def hemispheres_images(browser):
     # deliverable 2 of Challenge
